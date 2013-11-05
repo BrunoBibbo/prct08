90907fb9b21e0087e1bdd4fb9e69a80a6fbc7eac
Bruno Bibbo <alu0100697063@ull.edu.es>
2013-11-05 15:53:02 +0000

    Se ha puesto en marcha el sistema Guard (se añade el fichero Guardfile). Se ha modificado el fichero de espectativas para incluir una prueba, da error dado que no hay ninguna implementación en la clase Matriz.

--- spec/Matriz_spec.rb
+++ spec/Matriz_spec.rb
@@ -1,4 +1,16 @@
 require "Matriz.rb"
 
 describe Matriz do
+
+	before :each do
+	@m1 = Matriz.random(3,3)
+	@m2 = Matriz.identidad(3,3)
+	end
+
+	describe "Comprobacion de matrices" do
+		it "Comprobando matriz identidad" do
+		@m2.to_s.should eq('[[1,0,0],[0,1,0],[0,0,1]]')
+		end
+	end
+
 end
