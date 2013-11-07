04fad61c1aa88a02d99d68d7346f691033ca263e
Ayoze Elvira <alu0100696974@ull.edu.es>
2013-11-07 13:01:11 +0000

    Se han añadido nuevas pruebas al fichero spec

--- spec/Matriz_spec.rb
+++ spec/Matriz_spec.rb
@@ -5,6 +5,7 @@ describe Matriz do
 	before :each do
 	@m1 = Matriz.random(3,3)
 	@m2 = Matriz.identidad(3,3)
+	@m6 = Matriz.identidad(3,3)
 	end
 
 	describe "Comprobacion de matrices" do
@@ -26,14 +27,24 @@ describe Matriz do
 			end        
 		end
 
-        	#it "La resta" do
-		#	@m4 = @m1 - @m2
- 		#	for i in 0...2
-		#		for j in 0...2
-		#		@m4[i][j].is_a? Integer
-		#		end
-		#	end        
-		#end
+		it "Suma identidad" do
+			@m5=@m2 + @m6
+			@m5.to_s.should eq("[[2,0,0][0,2,0][0,0,2]]")
+		end
+
+        	it "La resta" do
+			@m4 = @m1 - @m2
+ 			for i in 0...2
+				for j in 0...2
+				@m4[i][j].is_a? Integer
+				end
+			end        
+		end
+
+		it "Resta identidad" do
+			@m5=@m2 - @m6
+			@m5.to_s.should eq("[[0,0,0][0,0,0][0,0,0]]")
+		end
 
 	end
 end
